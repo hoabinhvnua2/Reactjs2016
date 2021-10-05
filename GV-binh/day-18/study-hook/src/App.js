9e8b7bdcfc8175491f32c430cf186f6ad5545fe7
hoabinhvnua <tranhoabinh.tha.hua@gmail.com>
2021-10-05 22:24:53 +0700

    day 18 and day 19

---
+++ GV-binh/day-18/study-hook/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.scss';
+import Count from './Count';
+import Products from './Products';
+
+function App() {
+  return (
+    <div className="App">
+      <Count />
+      <Products />
+    </div>
+  );
+}
+
+export default App;
