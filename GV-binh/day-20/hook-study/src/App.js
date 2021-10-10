408a27ad37e934450d2ab8394f9a69e50717697f
hoabinhvnua <tranhoabinh.tha.hua@gmail.com>
2021-10-10 21:58:27 +0700

    update day 20

---
+++ GV-binh/day-20/hook-study/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Tasks from './Tasks';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Tasks />
+    </div>
+  );
+}
+
+export default App;
